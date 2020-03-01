9e3639408d56698910bcd1781bed2beab78a35d1
ehsan <1374ea@gmail.com>
2020-03-01 22:14:00 +0330

    fix problem

--- python/python3.5-installer_ubuntu18.04.sh
+++ python/python3.5-installer_ubuntu18.04.sh
@@ -1,7 +1,7 @@
 cd /usr/src
 wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
 
-sudo tar xzf Python-3.5.6.tgz
+tar xzf Python-3.5.6.tgz
 
 cd Python-3.5.6
 sudo ./configure --enable-optimizations
