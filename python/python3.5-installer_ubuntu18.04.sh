749b50d7cbd2ad133e2514c03c942a2868f8dadd
ehsan <1374ea@gmail.com>
2020-03-02 09:38:46 +0330

    fix bug

--- python/python3.5-installer_ubuntu18.04.sh
+++ python/python3.5-installer_ubuntu18.04.sh
@@ -4,7 +4,7 @@ wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
 tar xzf Python-3.5.6.tgz
 
 cd Python-3.5.6
-sudo ./configure --enable-optimizations
-sudo make altinstall
+./configure --enable-optimizations
+make altinstall
 
 python3.5 -V
