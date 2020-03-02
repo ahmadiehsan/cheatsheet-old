f7c22d86c032bdceb39497c1b818d73975012885
ehsan <1374ea@gmail.com>
2020-03-02 11:38:59 +0330

    update python3.5 installer

--- python/python3.5-installer_debian-base.sh
+++ python/python3.5-installer_debian-base.sh
@@ -1,3 +1,5 @@
+apt install -y libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev libtk8.5 libgdm-dev libdb4o-cil-dev libpcap-dev
+
 cd /usr/src
 wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
 
